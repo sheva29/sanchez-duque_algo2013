e612a3a524fd95b1d1282681850f83f3168cc399
Mauricio Sanchez Duque <mauriciosanchezduque@server.local>
2013-09-11 13:59:56 -0400

    Fixed the video file

--- week2_a_video_drawing/src/testApp.cpp
+++ week2_a_video_drawing/src/testApp.cpp
@@ -2,77 +2,101 @@
 
 //--------------------------------------------------------------
 void testApp::setup(){
-
-	ofBackground(255, 255, 255);
+    
+    
+    video.loadMovie("fingers.mov");
+    video.setLoopState(OF_LOOP_NORMAL);
+    //video.setSpeed(0.4);
+    video.play();
 }
 
 //--------------------------------------------------------------
 void testApp::update(){
-
+    
+    video.update();
 }
 
 //--------------------------------------------------------------
 void testApp::draw(){
-
-	
-	ofDrawBitmapString("take a look at the console / in xcode, command-shift-r", ofPoint(30,30));
-	
-	ofSetColor(0,0,0);
-	ofNoFill();
-	ofBeginShape();
-	for (int i = 0; i < pts.size();i++){
-		ofVertex(pts[i].x, pts[i].y);
-	}
-	ofEndShape();
+    
+    ofEnableAlphaBlending();
+    ofSetColor(255,255,255,180);
+    video.draw(0,0);
+    
+    ofSetLineWidth(3);
+    ofSetColor(255,0,0);
+    TS.draw(video.getPosition());
+    for (int i = 0; i < strokes.size(); i++){
+        strokes[i].draw(video.getPosition());
+    }
+    
+    ofDrawBitmapStringHighlight("press ' ' to clear", ofPoint(video.getWidth() + 50, 50), ofColor::magenta, ofColor::white);
+    ofDrawBitmapStringHighlight("press a to reset video position", ofPoint(video.getWidth() + 50, 80), ofColor::magenta, ofColor::white);
+    ofDrawBitmapStringHighlight("press + to increase video speed", ofPoint(video.getWidth() + 50, 110), ofColor::magenta, ofColor::white);
+    ofDrawBitmapStringHighlight("press - to increase video speed", ofPoint(video.getWidth() + 50, 140), ofColor::magenta, ofColor::white);
+    ofDrawBitmapStringHighlight("video speed: " + ofToString(video.getSpeed(), 3), ofPoint(video.getWidth() + 50, 170), ofColor::magenta, ofColor::white);
+    
 }
 
 //--------------------------------------------------------------
 void testApp::keyPressed(int key){
-
+    
+    if (key == ' '){
+        strokes.clear();
+        TS.pts.clear();
+    }
+    
+    if (key == 'a'){
+        video.setPosition(0);
+    }
+    
+    if (key == '+'){
+        video.setSpeed(video.getSpeed()+0.05);
+    }
+    
+    if (key == '-'){
+        video.setSpeed(video.getSpeed()-0.05);
+    }
 }
 
 //--------------------------------------------------------------
 void testApp::keyReleased(int key){
-
+    
 }
 
 //--------------------------------------------------------------
 void testApp::mouseMoved(int x, int y ){
-
+    
 }
 
 //--------------------------------------------------------------
 void testApp::mouseDragged(int x, int y, int button){
-
-	timePoint temp;
-	temp.x = x;
-	temp.y = y;
-	temp.t = ofGetElapsedTimef() - startTime;
-	pts.push_back(temp);
-	
-	printf("adding point %i at time %f \n", pts.size(), ofGetElapsedTimef() - startTime);
+    
+    TS.addPoint(video.getPosition(),ofPoint(x,y));
 }
 
 //--------------------------------------------------------------
 void testApp::mousePressed(int x, int y, int button){
-
-	pts.clear();
-	startTime = ofGetElapsedTimef();
-	timePoint temp;
-	temp.x = x;
-	temp.y = y;
-	temp.t = 0;
-	pts.push_back(temp);
-	
+    
 }
 
 //--------------------------------------------------------------
 void testApp::mouseReleased(int x, int y, int button){
-
+    
+    strokes.push_back(TS);
 }
 
 //--------------------------------------------------------------
 void testApp::windowResized(int w, int h){
-
+    
 }
 
+//--------------------------------------------------------------
+void testApp::gotMessage(ofMessage msg){
+    
+}
+
+//--------------------------------------------------------------
+void testApp::dragEvent(ofDragInfo dragInfo){
+    
+}
--- week2_a_video_drawing/src/testApp.h
+++ week2_a_video_drawing/src/testApp.h
@@ -1,33 +1,63 @@
-#ifndef _TEST_APP
-#define _TEST_APP
-
+#pragma once
 
 #include "ofMain.h"
-#include "timePoint.h"
 
+//struct as a mini class
+typedef struct {
+    
+    float timePct; // pct is based on movie position;
+    ofPoint pos; // position of the point in x and y
+    
+} timePoint;
 
-
-
-class testApp : public ofBaseApp{
-
-	public:
-		void setup();
-		void update();
-		void draw();
-
-		void keyPressed  (int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void windowResized(int w, int h);
-
-		
-		vector < timePoint > pts;
-		float startTime;
-	
-	
+class timeStroke {
+public:
+    void addPoint(float pct, ofPoint pos){
+        //We create a new variable of our struct. And we assign them to our variables int he constructor and add them to our vector
+        timePoint temp;
+        temp.timePct = pct;
+        temp.pos = pos;
+        pts.push_back(temp);
+    }
+    
+    void draw(float pct){
+        
+        if (pts.size() > 0){
+            for (int i = 0; i < pts.size()-1; i++){
+                if (pts[i].timePct < pct && pts[i+1].timePct < pct){
+                    ofLine(pts[i].pos, pts[i+1].pos);
+                }
+            }
+        }
+    }
+    
+    vector < timePoint > pts;
+    
 };
 
-#endif
+class testApp : public ofBaseApp{
+    
+public:
+    void setup();
+    void update();
+    void draw();
+    
+    void keyPressed  (int key);
+    void keyReleased(int key);
+    void mouseMoved(int x, int y );
+    void mouseDragged(int x, int y, int button);
+    void mousePressed(int x, int y, int button);
+    void mouseReleased(int x, int y, int button);
+    void windowResized(int w, int h);
+    void dragEvent(ofDragInfo dragInfo);
+    void gotMessage(ofMessage msg);
+    
+    
+    ofVideoPlayer video;
+    
+    //This class takes our little struct called timePoint. and then a TS class is created
+    timeStroke TS;
+    vector < timeStroke > strokes;
+    
+    
+};
