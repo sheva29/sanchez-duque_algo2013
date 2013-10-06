41b33c82dd8b916edccf117e6db7f68ee373cdb0
sheva29 <design@sanchezmauricio.com>
2013-10-06 14:51:56 -0400

    Merging to assignments

---
+++ week5_1&2_GUI_settings_control_using_particles/src/testApp.cpp
@@ -0,0 +1,193 @@
+#include "testApp.h"
+
+//--------------------------------------------------------------
+void testApp::setup(){
+    
+    ofSetVerticalSync(true);
+    ofSetBackgroundAuto(true);
+  
+ 
+    //Let's do our GUI elements
+    
+    //We initiate our pointer
+    gui = new ofxUICanvas;
+    gui->addLabel("Controller");
+    gui->addSpacer();
+    gui->addButton("makeRound", false, 44,44);
+    gui->setColorBack(ofColor(200));
+    gui->setWidgetColor(OFX_UI_WIDGET_COLOR_BACK, ofColor(255,100));
+    gui->addSlider("particleSize",0, 10,2);
+    
+    ofAddListener(gui->newGUIEvent, this, &testApp::onGuiEvent);
+
+    gui->loadSettings("guiSettings.xml");
+    
+    
+    //Let's work with our image
+    
+    bitchPls.loadImage("rdjr.jpg");
+    
+    int xRes = floor( bitchPls.getWidth()  / 20.0 );
+    int yRes = floor( bitchPls.getHeight() /  20.0 );
+    
+    for ( int x = 0; x < xRes; x++){
+        for( int y = 0; y < yRes; y++){
+            
+            addParticle(x,y);
+            
+        }
+    }
+    
+    ofPopMatrix();
+    
+    
+
+    
+    
+}
+
+//--------------------------------------------------------------
+void testApp::exit(){
+    
+    gui->saveSettings("guiSetting.xml");
+    delete gui;
+    
+}
+
+//--------------------------------------------------------------
+void testApp::onGuiEvent(ofxUIEventArgs &e){
+    
+    string name = e.widget->getName();
+    int kind = e.widget->getKind();
+    
+    if( name == "particleSize"){
+        
+        ofxUISlider *particleSize= (ofxUISlider *)e.widget;
+        
+     
+        vector < Particles >::iterator it;
+        for( it = imgParticles.begin(); it != imgParticles.end(); ++it){
+            
+            //Here we assigned our value to the slider
+            it->rectSizeMax = particleSize->getScaledValue();
+            
+            cout << " Got a message!" << name << " - " << particleSize->getValue() << endl;
+        }
+        
+//Using a regular forloop
+//        for( int i = 0; i < imgParticles.size(); i++){
+//            
+//            imgParticles[i].rectSizeMax = particleSize->getScaledValue();
+//             cout << " Got a message!" << name << " - " << particleSize->getValue() << endl;
+//        }
+        
+    }
+    
+    
+}
+//--------------------------------------------------------------
+void testApp::addParticle(float x, float y){
+    
+//    float xPos = (x + 0.5f) * 10.0;
+//    float yPos = (y + 0.5f) * 10.0;
+    
+    float xPos = x * 20;
+    float yPos = y * 20;
+    
+    Particles tmp( ofVec2f( xPos, yPos));
+    imgParticles.push_back(tmp);
+    
+    
+}
+
+//--------------------------------------------------------------
+void testApp::update(){
+   
+    vector< Particles >::iterator it;
+    for( it = imgParticles.begin(); it != imgParticles.end(); ++it){
+        
+        (*it).update(bitchPls);
+    }
+    
+    
+//    for( int i =0; i < imgParticles.size(); i++){
+//        
+//        imgParticles[i].update( bitchPls/*, rectMax*/ );
+//    }
+    
+    
+}
+
+//--------------------------------------------------------------
+void testApp::draw(){
+    
+    
+    //    for( vector<Particles>::iterator it = imgParticles.begin(); it < imgParticles.end(); it++){
+    //
+    //        (*it).draw(bitchPls);
+    //    }
+    
+    //
+    
+    vector<Particles>::iterator it;
+    for( it = imgParticles.begin(); it != imgParticles.end(); ++it){
+            
+//            ofRotate(45);
+            it->draw();
+        
+    }
+
+    
+//    for( int i = 0; i < imgParticles.size(); i++){
+//        
+//        imgParticles[i].draw();
+//    }
+
+    
+    
+}
+
+//--------------------------------------------------------------
+void testApp::keyPressed(int key){
+    
+}
+
+//--------------------------------------------------------------
+void testApp::keyReleased(int key){
+    
+}
+
+//--------------------------------------------------------------
+void testApp::mouseMoved(int x, int y ){
+    
+}
+
+//--------------------------------------------------------------
+void testApp::mouseDragged(int x, int y, int button){
+    
+}
+
+//--------------------------------------------------------------
+void testApp::mousePressed(int x, int y, int button){
+    
+}
+
+//--------------------------------------------------------------
+void testApp::mouseReleased(int x, int y, int button){
+    
+}
+
+//--------------------------------------------------------------
+void testApp::windowResized(int w, int h){
+    
+}
+
+//--------------------------------------------------------------
+void testApp::gotMessage(ofMessage msg){
+    
+}
+
+//--------------------------------------------------------------
+void testApp::dragEvent(ofDragInfo dragInfo){ 
+    
+}
